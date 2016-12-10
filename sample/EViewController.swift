00903cf979d6af86fe853a8943ee8d1b37d4bc80
Min Aung Hein <minaunghein@gmail.com>
2016-12-10 20:28:43 +0630

    This is initial

--- sample/EViewController.swift
+++ sample/EViewController.swift
@@ -14,6 +14,7 @@ class EViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        //This is edited by MAH
     }
 
     override func didReceiveMemoryWarning() {
